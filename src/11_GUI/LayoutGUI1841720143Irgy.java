b49b9b53129843a5d67b3d0edb3a155f2f435204
MIrgyPratama <54735731+MIrgyPratama@users.noreply.github.com>
2019-11-11 21:19:50 +0700

    Add files via upload

---
+++ src/11_GUI/LayoutGUI1841720143Irgy.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manajemenlayout;
+
+/**
+ *
+ * @author IRGY
+ */
+import javax.swing.JFrame;
+public class LayoutGUI1841720143Irgy {
+    public static void main(String[] args) {
+        JFrame frame = new Border1841720143Irgy();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+        JFrame frame2 = new Grid1841720143Irgy();
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame2.setVisible(true);
+        
+        JFrame frame3 = new Box1841720143Irgy();
+        frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame3.setVisible(true);
+    }
+}
+
+
