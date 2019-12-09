fcae7123e674377120b95b72e0c674df39281036
MIrgyPratama <54735731+MIrgyPratama@users.noreply.github.com>
2019-12-10 01:13:07 +0700

    Add files via upload

---
+++ src/15_Unit_Testing/unittes/LearnUnitTesting1841720143Irgy.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unittes;
+
+/**
+ *
+ * @author IRGY
+ */
+public class LearnUnitTesting1841720143Irgy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MessageProcessor1841720143Irgy mp = new MessageProcessor1841720143Irgy();
+        mp.setSender("Ronaldo");
+        mp.setRecipient("Rafael");
+        mp.setMessage("Whats up bro?");
+        mp.showMessage();
+    }
+
+}
